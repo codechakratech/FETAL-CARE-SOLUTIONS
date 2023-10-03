47dcf1789f1a7203a2d77665ac25e18dd9c7e68a
codechakratech <81754948+codechakratech@users.noreply.github.com>
2023-10-03 21:50:38 +0530

    Update user.js chanes firstname and lastname

--- Models/user.js
+++ Models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
-    name:{
+    firstname:{
+        type:String,
+        required:true
+    },
+      lastname:{
         type:String,
         required:true
     },
