1c9aa9617ebced85be6251264eb26737d1e31730
codechakratech <81754948+codechakratech@users.noreply.github.com>
2023-10-04 15:06:25 +0530

    Update dcaController.js

--- controller/dcaController.js
+++ controller/dcaController.js
@@ -3,7 +3,7 @@ const Dca = require('../Models/dcaTool')
 exports.getDca = async (req,res)=>{
     try {
         const data = await Dca.find()
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})
     }
@@ -12,7 +12,7 @@ exports.getDca = async (req,res)=>{
 exports.postDca = async (req,res)=>{
     try {
         const data = await Dca.create(req.body)
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
 
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})        
@@ -23,7 +23,7 @@ exports.postDca = async (req,res)=>{
 exports.putDca = async (req,res)=>{
     try {
         const data = await Dca.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})                
     }
@@ -32,7 +32,7 @@ exports.putDca = async (req,res)=>{
 exports.deleteDca = async (req,res)=>{
     try {
         const data = await Dca.findByIdAndDelete(req.params.id)
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})                  
     }
