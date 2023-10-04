f6d513e15ba2caa7ac1044a56b69d93efbdbe2f0
codechakratech <81754948+codechakratech@users.noreply.github.com>
2023-10-04 15:05:42 +0530

    Update pasController.js

--- controller/pasController.js
+++ controller/pasController.js
@@ -3,16 +3,16 @@ const Pas = require('../Models/Pas')
 exports.getPas = async (req,res)=>{
     try {
         const data = await Pas.find()
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
     } catch (error) {
-        return res.status(400).json({errors:true,message:error.message})
+        return res.json({errors:true,message:error.message})
     }
 }
 
 exports.postPas = async (req,res)=>{
     try {
         const data = await Pas.create(req.body)
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
 
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})        
@@ -23,7 +23,7 @@ exports.postPas = async (req,res)=>{
 exports.putPas = async (req,res)=>{
     try {
         const data = await Pas.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})                
     }
@@ -32,7 +32,7 @@ exports.putPas = async (req,res)=>{
 exports.deletePas = async (req,res)=>{
     try {
         const data = await Pas.findByIdAndDelete(req.params.id)
-        return res.status(400).json({errors:false,data:data})
+        return res.json({errors:false,data:data})
     } catch (error) {
         return res.status(400).json({errors:true,message:error.message})                  
     }
