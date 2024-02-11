0c1831401e86a6f82c99b367de99a0ac226624b0
codechakratech <81754948+codechakratech@users.noreply.github.com>
2024-02-12 01:36:10 +0530

    Update userController.js put user password

--- controller/userController.js
+++ controller/userController.js
@@ -44,6 +44,21 @@ exports.login = async (req,res)=>{
     }
 }
 
+exports.putUserPassword = async (req,res)=>{
+    try {
+        const userExists = await User.findOne({email:req.body.email})
+        if(!userExists) return res.status(400).json({errors:true,message:'email or password invalid'})
+        
+         const sault = await bcrypt.genSalt()
+        req.body.password = await bcrypt.hash(req.body.password,sault)
+        
+        const data = await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        return res.json({errors:false,data:data})
+    } catch (error) {
+        return res.status(400).json({errors:true,message:error.message})                
+    }
+}
+
 exports.putUser = async (req,res)=>{
     try {
         const data = await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
